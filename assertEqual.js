dfda93453ef8ed93f02a57bcaf5c11709085578e
@syabdulr <abdulsyed1998@gmail.com>
2023-05-01 16:35:39 -0400

    Created a function that compares two arguments

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log("🟢🟢🟢 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🔴🔴🔴 Assertion Failed: " + actual + " != " + expected);
+  }
+
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1,2);
