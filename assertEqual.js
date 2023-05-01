b9d6de4a4945d3f0ccd1a3947575aaeba3a8f6a2
@syabdulr <abdulsyed1998@gmail.com>
2023-05-01 16:48:35 -0400

    rewrite code for better efficiency

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log("🟢🟢🟢 Assertion Passed: " + actual + " === " + expected);
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual}  === ${expected}`);
   } else {
-    console.log("🔴🔴🔴 Assertion Failed: " + actual + " != " + expected);
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual}  !=  ${expected}`);
   }
 
 };
