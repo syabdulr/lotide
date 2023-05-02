9b588846fc307b85eba362da63b7884ab9968b7c
@syabdulr <abdulsyed1998@gmail.com>
2023-05-02 19:13:14 -0400

    add script that compares if two arrays are equal in length as well as the individual elements within them

---
+++ eqArrays.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual}  === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual}  !=  ${expected}`);
+  }
+
+};
+
+const eqArrays = function(array1, array2){
+
+  if(array1.length !== array2.length){
+    return false;
+  }
+
+  for(let x = 0; x <= array1.length; x++){
+    if (array1[x] !== array2[x]){
+      return false;
+    } 
+    
+  }
+
+  return true;
+
+
+}
+
+
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
