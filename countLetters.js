fea81983582634b2cafa6cfe8678affbb3ac8157
@syabdulr <abdulsyed1998@gmail.com>
2023-05-03 19:06:50 -0400

    add script that counts letters from a string

---
+++ countLetters.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual}  === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual}  !=  ${expected}`);
+  }
+
+};
+
+const countLetters = function(string){
+  //take in a sentance as a string
+  // return a count of each letter in that sentance
+
+  //the return value should be an object
+  let letterCount = {};
+
+  for(let count of string) {
+
+    if(count !== ' ' && count !== '.'){
+
+      if(letterCount[count]){
+    
+        letterCount[count]++
+  
+      } else {
+        letterCount[count] = 1;
+      }
+    }
+    
+  }
+
+  console.log(letterCount);
+  return letterCount;
+}
+
+countLetters("This is a sentence.");
