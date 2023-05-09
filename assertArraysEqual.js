8e10a1f37b0ac62fbcf2f4b07610e8780fd931c6
@syabdulr <abdulsyed1998@gmail.com>
2023-05-08 20:30:44 -0400

    refactor code and add a test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,27 +1,9 @@
-const eqArrays = function(array1, array2){
-
-  if(array1.length !== array2.length){
-    return false;
-  }
-
-  for(let x = 0; x < array1.length; x++){
-    if (array1[x] !== array2[x]){
-      return false;
-    } 
-    
-  }
-
-  return true;
-
-}
-
+const eqArrays = require('./eqArrays'); // import eqArrays
 
 
 const assertArraysEqual = function(array1, array2){
 
-  let result = eqArrays(array1,array2); //store the value of whether the check is true or false
-
-  if(result === true){
+  if(eqArrays(array1,array2)){
     console.log(`🟢🟢🟢 Assertion Passed: ${array1}  === ${array2}`);
   } else {
       console.log(`🔴🔴🔴 Assertion Failed: ${array1}  !== ${array2}`);
@@ -29,13 +11,4 @@ const assertArraysEqual = function(array1, array2){
 
 }
 
-const middle = function(array){
-  //take an array and print it
-  console.log(array);
-}
-
-let middleValue = [1,2,3,4,5];
-
-
-//assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-//assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+module.exports = assertArraysEqual;
